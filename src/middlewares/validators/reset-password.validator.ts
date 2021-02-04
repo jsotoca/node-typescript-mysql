9af4f6d9b9ae5bc8fc89663041c578b4770ca546
jsotoca <jsotoca@unprg.edu.pe>
2021-02-03 23:47:17 -0500

    validacion de params de resetear contraseña agregado

--- src/middlewares/validators/reset-password.validator.ts
+++ src/middlewares/validators/reset-password.validator.ts
@@ -1,5 +1,7 @@
-import { body } from 'express-validator';
+import { body, param } from 'express-validator';
 
 export const resetPasswordValidate = [
-    body('password','La contraseña es requerida.').exists()
+    param('email','El email es necesario.').exists().isString().isEmail(),
+    param('token','El token es necesario.').exists().isString(),
+    body('password','La contraseña es requerida.').exists().isString()
 ];
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -4,6 +4,7 @@ import { uploadSingle } from '../helpers/upload.helper';
 import { confirmationValidator } from '../middlewares/validators/confirmation.validator';
 import { CreateUserValidator } from '../middlewares/validators/create-user.validator';
 import { forgotPasswordValidator } from '../middlewares/validators/forgot-password.validator';
+import { resetPasswordValidate } from '../middlewares/validators/reset-password.validator';
 import { signInValidate } from '../middlewares/validators/signin.validator';
 import { validationResults } from '../middlewares/validators/Validationresults.validator';
 
@@ -13,6 +14,6 @@ userRouter.post('/', uploadSingle('avatar'), CreateUserValidator, validationResu
 userRouter.post('/login', signInValidate, validationResults, signIn);
 userRouter.get('/confirmation/:email/:token', confirmationValidator, validationResults, verifiedAccount);
 userRouter.get('/forgot-password/:email',forgotPasswordValidator, validationResults, forgotPassword);
-userRouter.post('/reset-password/:email/:token', resetPassword);
+userRouter.post('/reset-password/:email/:token',resetPasswordValidate, validationResults, resetPassword);
 
 export default userRouter;
